1ca15a17420540945e9e8dad22b2d07f9043c0ed
Mae <mae6666666666>
2026-03-08 14:18:54 +0000

    Finised making the agentic ai of the AI video verifier

--- main.py
+++ main.py
@@ -6,11 +6,9 @@ from openai import OpenAI
 
 client = OpenAI(api_key=API_KEY, base_url=BASE_URL)
 
-GORILLAZ_GENERATOR ={
-    "name": "Gorillaz",
-    "system_prompt": '''You are an expert in all information revolving around the band Gorillaz. You know 
-    at least 5 about each band member, every album released, and other little things about the band such as 
-    promotions they have done, collaborations, and music videos promoting said collaboration'''
+AI_DESCRIPTION ={
+    "name": "AI Video Verifier",
+    "system_prompt": '''You are an AI trained to specifically analyse descriptions given by another AI agent which previously analysed the video and turned it into json for you to read and verify, and confirm the likeliness of them being AI generated. Use tools and the internet to help you. If the prompt involves verifying if a movie is fake, for example asking if a movie trailer is legit, use the internet to check it. DO NOT use your own knowledge'''
 
 }
 
@@ -47,12 +45,12 @@ def web_search(query):
 
 
 
-def gorillaz_fact_checker(fact):
+def gorillaz_fact_checker(video_json):
     print("-" * 50)
 
     messages = [
-        {"role": "system", "content": GORILLAZ_GENERATOR["system_prompt"]},
-        {"role": "user", "content": f"verify if this fact is real. you can use tools, use the internet to fact check: {fact} DO NOT use your own knowledge. if you cant verify via the internet, simply say you don't know"}
+        {"role": "system", "content": AI_DESCRIPTION["system_prompt"]},
+        {"role": "user", "content": f"Please verify the json of this video and tell me if it is real or likely AI generated as a percentage: {video_json}"}
     ]
 
     max_tool_calls = 5  # Safety limit so it doesn't loop forever
@@ -97,5 +95,42 @@ def gorillaz_fact_checker(fact):
         print("[Hit max tool calls -- stopping]")
 
 
-gorillaz_fact_checker("Phase three of the band Gorillaz is titled Plastic Beach")
+
+
+video0 = {
+    "title": "Cat playing saxophone",
+    "extra_limbs": True,
+    "innacurate_lighting": True,
+    "unnatural_animal_behaviour": True,
+    "grainy_off_sounding_voices": False
+}
+
+video1 = {
+    "title": "Dog playing with a frizbee",
+    "extra_limbs": False,
+    "innacurate_lighting": True,
+    "unnatural_animal_behaviour": False,
+    "grainy_off_sounding_voices": False
+}
+
+video2 = {
+    "title": "Damon Albarn giving away free phones",
+    "extra_limbs": False,
+    "innacurate_lighting": True,
+    "unnatural_facial_expressions": True,
+    "off_lip_syncing": True,
+    "odd_sounding_audio": True
+}
+
+video3 = {
+    "title": "Masters Of The Universe Trailer",
+    "extra_limbs": False,
+    "innacurate_lighting": False,
+    "unnatural_facial_expressions": False,
+    "off_lip_syncing": False,
+    "odd_sounding_audio": False
+}
+
+
+gorillaz_fact_checker(video3)
 
